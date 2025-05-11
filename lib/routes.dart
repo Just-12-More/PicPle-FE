03f8b19b8d9958639bfbd693439e316e02a88608
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-05-11 19:35:57 +0900

    Feat: 라우터 경로 eum으로 관리
    Co-authored-by: Donghyeon Kim <kimdonghyun90766@gmail.com>

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,10 +3,21 @@ import 'package:picple/presentation/home/view/home_page.dart';
 import 'package:picple/presentation/login/view/login_page.dart';
 import 'package:picple/presentation/splash/view/splash_page.dart';
 
+enum Routes {
+  splash(name: 'Splash', path: '/'),
+  home(name: 'Home', path: '/home'),
+  login(name: 'Login', path: '/login');
+
+  final String name;
+  final String path;
+
+  const Routes({required this.name, required this.path});
+}
+
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const SplashPage()),
-    GoRoute(path: '/home', builder: (context, state) => const HomePage()),
-    GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
+    GoRoute(path: Routes.splash.path, builder: (context, state) => const SplashPage()),
+    GoRoute(path: Routes.home.path, builder: (context, state) => const HomePage()),
+    GoRoute(path: Routes.login.path, builder: (context, state) => const LoginPage()),
   ],
 );
