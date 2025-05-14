7af929de5314851adab6aae7b1b2f224e4589abf
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-14 13:50:22 +0900

    Feat/#8 : PicpleColors 정의

---
+++ lib/presentation/theme/picple_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PicpleColors {
+  static const Color primary1 = Color(0xFF1778FB);
+  static const Color primary2 = Color(0xFFE1E1E1);
+
+  static const Color secondary1 = Color(0xFF0BA4B6);
+  static const Color secondary2 = Color(0xFFE7F6F8);
+
+  static const Color gray1 = Color(0xFFF9FAFB);
+  static const Color gray2 = Color(0xFFF2F4F6);
+  static const Color gray3 = Color(0xFFE5E8EB);
+  static const Color gray4 = Color(0xFFB0B8C1);
+  static const Color gray5 = Color(0xFF6B7684);
+
+  static const Color red = Color(0xFFFF5761);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color background2 = Color(0xFFFEF7FF);
+}
