1079bcc62b3951bf39bcf3cebb2b000910d0d0ed
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-05-17 00:08:12 +0900

    Feat: 토큰 관리를 위한 Storage 추가

--- lib/data/locator.dart
+++ lib/data/locator.dart
@@ -1,10 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:picple/data/dio_client.dart';
 import 'package:picple/services/auth_service.dart';
+import 'package:picple/services/storage_service.dart';
 
 final locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => DioClient());
-  locator.registerLazySingleton(() => AuthService(locator<DioClient>()));
+  locator.registerLazySingleton(() => StorageService());
+  locator.registerLazySingleton(() => AuthService(locator<DioClient>(), locator<StorageService>()));
 }
