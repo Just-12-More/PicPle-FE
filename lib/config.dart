f5692e01d794a90dfccbe63ebf2b6f45aa78109d
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-05-16 21:01:26 +0900

    Chore: Config에 base url 추가

--- lib/config.dart
+++ lib/config.dart
@@ -3,9 +3,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Config {
   static late final String naverClientId;
   static late final String kakaoNativeAppKey;
+  static late final String baseUrl;
 
   static Future<void> init() async {
     await dotenv.load(fileName: "assets/config/.env");
+
+    baseUrl = dotenv.env['BASE_URL'] as String;
     naverClientId = dotenv.env['NAVER_CLIENT_ID'] as String;
     kakaoNativeAppKey = dotenv.env['KAKAO_NATIVE_APP_KEY'] as String;
   }
