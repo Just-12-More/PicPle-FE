4dbb26131f78d4383777372e348a057a0bb78fcf
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-21 20:21:57 +0900

    Feat/#22 : AccessToken 가지고 있을 시 자동 로그인 기능 구현

--- lib/presentation/splash/provider/splash_notifier.dart
+++ lib/presentation/splash/provider/splash_notifier.dart
@@ -1,23 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:picple/data/service_providers.dart';
 import 'package:picple/presentation/splash/provider/splash_contract.dart';
+
+import '../../../data/api/storage_api.dart';
+import '../../../routes.dart';
 
 final splashStateProvider = NotifierProvider<SplashNotifier, SplashState>(() => SplashNotifier());
 final splashEffectProvider = StateProvider<SplashEffect?>((ref) => null);
 
 class SplashNotifier extends Notifier<SplashState> {
+  late final StorageApi _storageApi;
+
   @override
   SplashState build() {
+    _storageApi = ref.watch(storageApiProvider);
     return SplashState();
   }
 
-  void checkLogin() {
-    Future.delayed(const Duration(seconds: 2), () {
-      if (false) {
-        _navigateTo('/home');
-      } else {
-        _navigateTo('/login');
-      }
-    });
+  void checkLogin() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    final accessToken = await _storageApi.getAccessToken() ?? '';
+    if (accessToken.isNotEmpty) {
+      _navigateTo(Routes.home.path);
+    } else {
+      _navigateTo(Routes.login.path);
+    }
   }
 
   void _navigateTo(String route) {
