30e70c16e74406b232850470f7860e5268044d63
dongchyeon <kimdonghyun90766@gmail.com>
2025-05-23 11:52:34 +0900

    Feat/#25 : BottomNavigationBar 디자인 변경

--- lib/presentation/component/picple_bottom_navigation_bar.dart
+++ lib/presentation/component/picple_bottom_navigation_bar.dart
@@ -6,35 +6,96 @@ import 'package:picple/presentation/theme/picple_typography.dart';
 class PicpleBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
+  final VoidCallback onUploadTap;
 
   const PicpleBottomNavigationBar({
     super.key,
     required this.currentIndex,
     required this.onTap,
+    required this.onUploadTap,
   });
 
-  Widget _buildNavItem(
-      {required int index,
-        required String iconPath,
-        required String label}) {
-    final isSelected = currentIndex == index;
-    return Expanded(
-      child: InkResponse(
-        focusColor: Colors.transparent,
-        hoverColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        onTap: () => onTap(index),
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          BottomAppBar(
+            color: PicpleColors.white,
+            shape: const CircularNotchedRectangle(),
+            notchMargin: 8,
+            child: SizedBox(
+              height: 76,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildNavItem(
+                    index: 0,
+                    iconPath: 'assets/icons/ic_home.svg',
+                    label: '홈',
+                    isSelected: currentIndex == 0,
+                    onTap: () => onTap(0),
+                  ),
+                  const SizedBox(width: 72), // 카메라 버튼 공간 확보
+                  _buildNavItem(
+                    index: 1,
+                    iconPath: 'assets/icons/ic_profile.svg',
+                    label: '프로필',
+                    isSelected: currentIndex == 1,
+                    onTap: () => onTap(1),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      
+          // 가운데 카메라 버튼
+          Positioned(
+            top: 0,
+            bottom: 0, // 높이 조정
+            child: SizedBox(
+              width: 64,
+              height: 64,
+              child: FloatingActionButton(
+                onPressed: onUploadTap,
+                backgroundColor: PicpleColors.primary1,
+                shape: const CircleBorder(),
+                elevation: 0,
+                child: SvgPicture.asset(
+                  'assets/icons/ic_upload.svg',
+                  width: 48,
+                  height: 48,
+                  colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required int index,
+    required String iconPath,
+    required String label,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: 80,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SvgPicture.asset(
               iconPath,
               width: 24,
               height: 24,
               colorFilter: ColorFilter.mode(
-                isSelected
-                    ? PicpleColors.primary1
-                    : PicpleColors.gray5,
+                isSelected ? PicpleColors.primary1 : PicpleColors.gray5,
                 BlendMode.srcIn,
               ),
             ),
@@ -44,37 +105,6 @@ class PicpleBottomNavigationBar extends StatelessWidget {
               style: PicpleTypography.nav.copyWith(
                 color: isSelected ? PicpleColors.primary1 : PicpleColors.gray5,
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: Colors.white,
-      height: 77,
-      shadowColor: Colors.black,
-      child: SizedBox(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _buildNavItem(
-              index: 0,
-              iconPath: 'assets/icons/ic_home.svg',
-              label: '홈',
-            ),
-            _buildNavItem(
-              index: 1,
-              iconPath: 'assets/icons/ic_upload.svg',
-              label: '사진 업로드',
-            ),
-            _buildNavItem(
-              index: 2,
-              iconPath: 'assets/icons/ic_profile.svg',
-              label: '프로필',
             ),
           ],
         ),
